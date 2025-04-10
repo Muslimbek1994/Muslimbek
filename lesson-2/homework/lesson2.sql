cfbda108ad0f30b11aceec70908463350fa92d8d
Muslimbek1994 <muslimbek.mirzabdullayev@gmail.com>
2025-04-09 19:21:30 -0500

    Create lesson2.sql

---
+++ lesson-2/homework/lesson2.sql
@@ -0,0 +1,7 @@
+--Easy
+/*1. Create a table `Employees` with columns: `EmpID` INT, `Name` (VARCHAR(50)), and `Salary` (DECIMAL(10,2))*/
+CREATE TABLE Employees (
+    EmpID INT,
+    Name VARCHAR(50),
+    Salary DECIMAL(10,2)
+);
